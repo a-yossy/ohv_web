de5a5133510c20f2b25df9a38ce6fb9c63e05d16
atsuya02 <atsuya0930@yahoo.co.jp>
2022-11-19 00:34:22 +0900

    feat: husky、lint-staged設定

---
+++ .lintstagedrc.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const buildEPrettierCommand = (filenames) =>
+  `yarn prettier --write ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' ')}`;
+
+module.exports = {
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildEPrettierCommand],
+  '*.{json,yml,yaml,md,css}': [buildEPrettierCommand],
+};
