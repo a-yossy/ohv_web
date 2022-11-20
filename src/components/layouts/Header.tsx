81b8349b67cd007b11e5d71cdcc04ae58028040a
atsuya02 <atsuya0930@yahoo.co.jp>
2022-11-20 17:14:50 +0900

    feat: レイアウト作成

---
+++ src/components/layouts/Header.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import styles from 'styles/components/layouts/Header.module.scss';
+
+export const Header: FC = () => (
+  <header className={styles.header}>
+    <>Outside Her Vision</>
+  </header>
+);
