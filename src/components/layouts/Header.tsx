2e40ba5ec8397a402cfcb664579c5e2fb88d4557
atsuya02 <atsuya0930@yahoo.co.jp>
2022-11-23 15:03:26 +0900

    style: ヘッダーのCSS修正

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,27 +1,33 @@
 import { FC } from 'react';
 import Link from 'next/link';
+import { FaBars } from 'react-icons/fa';
 import styles from 'styles/components/layouts/Header.module.scss';
 
 export const Header: FC = () => (
   <header className={styles.header}>
-    <div className={styles.left_links}>
-      <Link className={styles.logo} href='/'>
-        Outside Her Vision
-      </Link>
-    </div>
-    <div className={styles.right_links}>
-      <Link className={styles.right_link} href='/'>
-        BIO
-      </Link>
-      <Link className={styles.right_link} href='/live'>
-        LIVE
-      </Link>
-      <Link className={styles.right_link} href='/music'>
-        MUSIC
-      </Link>
-      <Link className={styles.right_link} href='/video'>
-        VIDEO
-      </Link>
+    <div className={styles.links}>
+      <div className={styles.lefts}>
+        <Link className={styles.logo} href='/'>
+          Outside Her Vision
+        </Link>
+      </div>
+      <div className={styles.rights}>
+        <button className={styles.no_decoration}>
+          <FaBars size={20} />
+        </button>
+        <Link className={styles.link} href='/'>
+          BIO
+        </Link>
+        <Link className={styles.link} href='/live'>
+          LIVE
+        </Link>
+        <Link className={styles.link} href='/music'>
+          MUSIC
+        </Link>
+        <Link className={styles.link} href='/video'>
+          VIDEO
+        </Link>
+      </div>
     </div>
   </header>
 );
