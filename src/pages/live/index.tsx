3f63d6caa1bfd5a38dbccf940f788859dab09dfc
a-yossy <atsuya0930@yahoo.co.jp>
2023-04-27 01:04:57 +0900

    feat: ページング機能実装

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -11,6 +11,7 @@ import {
   ALL_LIVES_QUERY_PARAMETER,
   MICRO_CMS_END_POINTS,
 } from 'src/features/constants/microCMS';
+import { useRouter } from 'next/router';
 
 type Props = {
   data: MicroCMSContents<Live>;
@@ -23,6 +24,15 @@ const Index: NextPage<Props> = ({ data }) => {
       compareDesc(new Date(a.performancedAt), new Date(b.performancedAt))
     );
 
+  const PER_PAGE = 9;
+  const TOTAL_PAGE = Math.ceil(lives.length / PER_PAGE);
+  const FIRST_PAGE = 1;
+  const router = useRouter();
+  const pageParam = Array.isArray(router.query.page)
+    ? router.query.page[0]
+    : router.query.page;
+  const page = Number(pageParam) || FIRST_PAGE;
+
   return (
     <>
       <Head>
@@ -30,7 +40,7 @@ const Index: NextPage<Props> = ({ data }) => {
         <meta name='description' content='ライブページ' />
       </Head>
       <Title>LIVE</Title>
-      {lives.map((live) => (
+      {lives.slice((page - 1) * PER_PAGE, page * PER_PAGE).map((live) => (
         <div key={live.id} className={styles.box}>
           <div className={styles.content}>
             <div className={styles.time_place}>
@@ -42,6 +52,27 @@ const Index: NextPage<Props> = ({ data }) => {
           </div>
         </div>
       ))}
+      <div className={styles.pager}>
+        <div className={styles.content}>
+          {page !== FIRST_PAGE && (
+            <button
+              className={styles.prev}
+              onClick={() => router.push(`${router.pathname}?page=${page - 1}`)}
+            >
+              前へ
+            </button>
+          )}
+          <span className={styles.page}>{`${page}ページ`}</span>
+          {page < TOTAL_PAGE && (
+            <button
+              className={styles.next}
+              onClick={() => router.push(`${router.pathname}?page=${page + 1}`)}
+            >
+              次へ
+            </button>
+          )}
+        </div>
+      </div>
     </>
   );
 };
