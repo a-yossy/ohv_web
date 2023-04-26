40b91b55c40b8f379f86958a98ed11b8377dec4a
a-yossy <atsuya0930@yahoo.co.jp>
2023-04-27 01:49:41 +0900

    feat: ogpタグ設定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,10 @@ const Top: NextPage = () => (
   <>
     <Head>
       <title>Outside Her Vision Official Website</title>
+      <meta name='og:title' content='Outside Her Vision Official Website' />
       <meta name='description' content='トップページ' />
+      <meta name='og:image' content='/public/top.jpg' />
+      <meta name='twitter:card' content='summary_large_image' />
     </Head>
     <div className={styles.image_container}>
       <Image className={styles.image} src={topImage} alt='トップページ画像' />
