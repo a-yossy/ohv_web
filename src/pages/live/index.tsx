53feee5b4d67699ca21b439bab67d368608f42ee
a-yossy <atsuya0930@yahoo.co.jp>
2023-05-17 00:24:21 +0900

    feat: リロードしてもソートされるよう修正

--- src/pages/live/index.tsx
+++ src/pages/live/index.tsx
@@ -12,18 +12,20 @@ import {
   MICRO_CMS_END_POINTS,
 } from 'src/features/constants/microCMS';
 import { useRouter } from 'next/router';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 
 type Props = {
   data: MicroCMSContents<Live>;
 };
 
 const Index: NextPage<Props> = ({ data }) => {
+  const router = useRouter();
   const lives = data.contents
     .slice()
     .sort((a, b) =>
       compareDesc(new Date(a.performancedAt), new Date(b.performancedAt))
     );
+
   const liveMonths = Array.from(
     new Set(
       lives
@@ -31,30 +33,42 @@ const Index: NextPage<Props> = ({ data }) => {
         .map((live) => format(new Date(live.performancedAt), 'yyyy-MM'))
     )
   );
+
+  const [sort, setSort] = useState<string>('all');
   const [sortedLives, setSortedLives] = useState(lives);
+
+  useEffect(() => {
+    const sortParam = Array.isArray(router.query.sort)
+      ? router.query.sort[0]
+      : router.query.sort;
+    if (sortParam === undefined) {
+      setSort('all');
+      setSortedLives(lives);
+    } else {
+      setSort(liveMonths.includes(sortParam) ? sortParam : 'all');
+      setSortedLives(
+        sort === 'all'
+          ? lives
+          : lives
+              .slice()
+              .filter(
+                (live) =>
+                  format(new Date(live.performancedAt), 'yyyy-MM') === sort
+              )
+      );
+    }
+  }, [router.query.sort, sort]);
 
   const PER_PAGE = 5;
   const TOTAL_PAGE = Math.ceil(sortedLives.length / PER_PAGE);
   const FIRST_PAGE = 1;
-  const router = useRouter();
   const pageParam = Array.isArray(router.query.page)
     ? router.query.page[0]
     : router.query.page;
-
   const page = Number(pageParam) || FIRST_PAGE;
 
   const handleChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const newLives =
-      e.target.value === 'all'
-        ? lives
-        : lives
-            .slice()
-            .filter(
-              (live) =>
-                format(new Date(live.performancedAt), 'yyyy-MM') ===
-                e.target.value
-            );
-    setSortedLives(newLives);
+    router.push(`${router.pathname}?sort=${e.target.value}`);
   };
 
   return (
@@ -64,7 +78,7 @@ const Index: NextPage<Props> = ({ data }) => {
         <meta name='description' content='ライブページ' />
       </Head>
       <Title>LIVE</Title>
-      <select onChange={(e) => handleChange(e)}>
+      <select onChange={(e) => handleChange(e)} value={sort}>
         <option value='all'>all</option>
         {liveMonths.map((liveMonth) => (
           <option value={liveMonth} key={liveMonth}>
@@ -89,7 +103,9 @@ const Index: NextPage<Props> = ({ data }) => {
           {page !== FIRST_PAGE && (
             <button
               className={styles.prev}
-              onClick={() => router.push(`${router.pathname}?page=${page - 1}`)}
+              onClick={() =>
+                router.push(`${router.pathname}?page=${page - 1}&sort=${sort}`)
+              }
             >
               前へ
             </button>
@@ -98,7 +114,9 @@ const Index: NextPage<Props> = ({ data }) => {
           {page < TOTAL_PAGE && (
             <button
               className={styles.next}
-              onClick={() => router.push(`${router.pathname}?page=${page + 1}`)}
+              onClick={() =>
+                router.push(`${router.pathname}?page=${page + 1}&sort=${sort}`)
+              }
             >
               次へ
             </button>
